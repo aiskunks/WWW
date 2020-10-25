e32ed4ab6f5d20049526cab1808ac13aadd08b0c
PinHoWang <abc8755987@gmail.com>
2020-10-25 08:42:07 -0400

    create base DOM

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className="top-section">Top Section</div>
+      <div className="page-section page-section--about">About</div>
+      <div className="page-section page-section--card">Card</div>
+      <div className="page-section page-section--slider">Slider</div>
+      <div className="page-section page-section--subscribe">Subscribe</div>
+      <footer className="footer">Footer</footer>
+      <div className="copyright">Copyright</div>
     </div>
   );
 }
